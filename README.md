3b650a8cf199f781a53d5dcfa9982439cd6ca789
F2b59 <f2b.fr59@gmail.com>
2023-11-08 21:11:04 +0200

    Web server / rev proxy / tileserver deployment working

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# bitcoin-events
-A map and list of Bitcoin-related notable events and conferences. Does not include regular meetups.
+# BitcoinEvents.com
+
+A map and list of Bitcoin-related notable events and conferences.
+
+In order to keep the map readable, regular meetups are not included. If you want to see local Bitcoin communities, you can have a look at [BTC Map - Communities](https://btcmap.org/communities).
+
+I'm limiting myself to European events for now. Also, events in France (and neighboring countries) may be more up to date than in the rest of Europe. Feel free to submit events and to update the map! Instructions on how to do it will be provided soon:tm:.
+
+## This repository
+
+For now, this repo contains an ansible playbook that deploys the web server, the (one-pager) website, and the tile server for the map.
+
+**/!&#92; Ongoing development. Things may very well be a bit broken. /!&#92;**
---
+++ roles/swag/files/www/app.js
@@ -0,0 +1,67 @@
+// Consts and Helper function definitions
+let url = "/data.json";
+let initView = {
+    "lat": 49.436,
+    "lng": 11.050,
+    "zoom": 5
+}
+
+// fetch events
+async function fetchJson(url) {
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error (`HTTP error: ${response.status}`);
+    }
+
+    // yeah, that part is kinda pointless if you fetch directly in Json and not Yaml
+    const jsonData = await response.json();
+    return jsonData;
+}
+
+function addEventToMap(event) {
+    let marker = L.marker([event.lat, event.lng]).addTo(map);
+    marker.bindPopup(eventDescription(event));
+}
+
+function eventDescription(event) {
+    let desc = `<span class=event-name><p><a href=${event.url}>${event.name}</a></p></span>`;
+    desc += `<p>${event.description}</p>`;
+    desc += `<p>Dates: ${event.dates[0]} - ${event.dates[1]}</p>`;
+    desc += `<p>Location: ${event.address}</p>`;
+    desc += `<p>Organized by ${event.organizer}</p>`;
+    return desc;
+}
+
+function setupLeaflet() {
+    // Wait for the document to be ready
+    document.addEventListener('DOMContentLoaded', function () {
+        const key = '1FAlXSwZpDAPrQYh9mvA'; // Maptiler API key
+        // Create a map instance and set the initial view coordinates and zoom level
+        map = L.map('map').setView([initView.lat, initView.lng], initView.zoom); // no "var" or "let" means the variable has global scope
+
+
+        // Add a tite layer to the map from OpenStreetMap
+        //L.tileLayer(`https://api.maptiler.com/maps/bright-v2/{z}/{x}/{y}.png?key=${key}`, {
+        //    attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e"
+        //}).addTo(map);
+        L.tileLayer(`/tileserver-gl/styles/bright/{z}/{x}/{y}.png`, {
+            attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e"
+        }).addTo(map);
+
+        // Create a TEST marker with popup and add it to the map
+        //let marker = L.marker([51.505, -0.09]).addTo(map);
+        //marker.bindPopup("Hello, I'm a marker!").openPopup();
+    });
+}
+
+async function main() {
+    setupLeaflet();
+    
+    const eventData = await fetchJson(url);
+    for (let i = 0; i < eventData.length; i++) {
+        addEventToMap(eventData[i]);
+    }
+
+}
+
+main();
